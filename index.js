4a6df8bdd1b7f515f5b9d810953648c80aafc58a
Md.Solaiman <md.solaiman366882@gmail.com>
2023-12-22 15:44:43 +0600

    Database connection established succesfully

--- index.js
+++ index.js
@@ -9,6 +9,50 @@ app.use(cors());
 app.use(express.json());
 
 
+// *************************** Database connection and operation Start **********************
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ea4znei.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Database Operations Start
+
+    const database = client.db("MTaskDB");
+    const taskCollection = database.collection("UserTask");
+
+    app.post('/task',async(req,res) =>{
+        const
+    })
+
+    // Database Operations End
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+// *************************** Database connection and operation End **********************
+
+
 
 
 //default setting for start the server
