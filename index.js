d48755fbd2e6b98c3e685fee76f13a17e6dfb3ad
Md.Solaiman <md.solaiman366882@gmail.com>
2023-12-22 19:54:33 +0600

    server deployed to vercel

--- index.js
+++ index.js
@@ -8,55 +8,73 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // *************************** Database connection and operation Start **********************
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ea4znei.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
 
-    // Database Operations Start
+		// Database Operations Start
 
-    const database = client.db("MTaskDB");
-    const taskCollection = database.collection("UserTask");
+		const database = client.db("MTaskDB");
+		const taskCollection = database.collection("UserTask");
 
-    app.post('/task',async(req,res) =>{
-        const newTask = req.body;
-        const result = await taskCollection.insertOne(newTask);
-        res.send(result)
-    });
+		//create new task
+		app.post("/task", async (req, res) => {
+			const newTask = req.body;
+			const result = await taskCollection.insertOne(newTask);
+			res.send(result);
+		});
 
+		//get specific user task
+		app.get("/task", async (req, res) => {
+			let query = {};
+			if (req.query?.email) {
+				query = {
+					email: req.query.email,
+				};
+			}
+            const cursor = taskCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+		});
 
-    // Database Operations End
+        //delete a task
+        app.delete("/task/:id",async(req,res) => {
+            const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await taskCollection.deleteOne(query);
+			res.send(result);
+        })
 
+		// Database Operations End
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		//await client.close();
+	}
 }
 run().catch(console.dir);
 
 // *************************** Database connection and operation End **********************
-
-
-
 
 //default setting for start the server
 app.get("/", (req, res) => {
